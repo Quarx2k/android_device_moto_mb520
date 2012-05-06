tpruvot/android_device_motorola_jordan

bootanimation res var, bootmenu addon fix and reduce recovery stable script diff
Change-Id: I14c6999ee5fc0976db8d517d5931e196cd1da3b9

cm.mk
@@ -14,11 +14,12 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/motorola/jordan/full_jordan.mk)
+TARGET_BOOTANIMATION_NAME := vertical-480x854
 
-# Inherit some common CM9 stuff.
 $(call inherit-product-if-exists, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product-if-exists, vendor/cm/config/gsm.mk)
+
+$(call inherit-product, device/motorola/jordan/full_jordan.mk)
 
 PRODUCT_NAME := cm_jordan
 PRODUCT_BRAND := MOTO

Quarx2k/android_device_moto_mb520

Conflicts:
	cm.mk

cm.mk
@@ -14,11 +14,14 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/moto/kobe/full_kobe.mk)
 
-# Inherit some common CM9 stuff.
+
+
+TARGET_BOOTANIMATION_NAME := vertical-480x854
+
 $(call inherit-product-if-exists, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product-if-exists, vendor/cm/config/gsm.mk)
+$(call inherit-product, device/moto/kobe/full_kobe.mk)
 
 PRODUCT_NAME := cm_kobe
 PRODUCT_BRAND := MOTO